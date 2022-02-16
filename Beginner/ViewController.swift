7f05ae2236a994a68cb2b38b50f6851624427fbe
Abdullah <iriandaabdullah546@gmail.com>
2022-02-16 07:58:05 +0700

    alert

--- Beginner/ViewController.swift
+++ Beginner/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnHello(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Massage", message: "Change Yout Life NOW!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
